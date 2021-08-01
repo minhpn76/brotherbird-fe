8a86a14818ab95e3d074f1fa639afdee4aa7404b
minhpn <minhpn@tinhvan.com>
2021-08-01 21:53:02 +0700

    clear cart when reload or close brower

--- src/redux/options.js
+++ src/redux/options.js
@@ -1,5 +1,25 @@
 import { AppReducerType } from "./types";
 import storage from "redux-persist/lib/storage";
+import { createTransform } from "redux-persist";
+import {ReduxState} from './types'
+
+const Transform = createTransform(
+  (inboundState, key) => {
+    if (key === AppReducerType.COLLECTION) {
+      return {
+        status: ReduxState.INIT,
+        ...inboundState,
+        cart: [],
+        itemRemoved: {},
+        tempCart: [],
+      };
+    }
+    return inboundState;
+  },
+  (outBoundState, key) => {
+    return outBoundState;
+  }
+);
 
 export const persistConfig = {
   timeout: process.env.NODE_ENV === "development" ? 0 : 30000,
@@ -12,5 +32,5 @@ export const persistConfig = {
     AppReducerType.COLLECTION,
     AppReducerType.LOADING_BAR,
   ],
-//   transforms: [Transform],
+  transforms: [Transform],
 };
